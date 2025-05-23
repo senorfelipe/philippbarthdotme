9d1c2b94a047e2d354261d506ad2ca0c8f3443cb
Philipp Barth <philipp-barth13@web.de>
2025-05-23 15:17:07 +0200

    some design changes

--- src/js/sideEffects.js
+++ src/js/sideEffects.js
@@ -14,4 +14,37 @@ function highlightCurrrentNavLink() {
   });
 }
 
+function openMenu() {
+  const menu = document.getElementById("mobileMenu");
+
+  console.log(menu);
+  if (!menu.classList.contains("open")) {
+    menu.classList.add("open");
+    return;
+  }
+}
+
+function closeMenu() {
+  const menu = document.getElementById("mobileMenu");
+  if (menu.classList.contains("open")) {
+    menu.classList.remove("open");
+  }
+}
+
 highlightCurrrentNavLink();
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", function () {
+  let currentScrollY = window.scrollY;
+
+  if (currentScrollY > lastScrollY) {
+    // Scrolling down → Hide navbar
+    document.getElementById("navbar").style.top = "-60px";
+  } else {
+    // Scrolling up → Show navbar
+    document.getElementById("navbar").style.top = "0px";
+  }
+
+  lastScrollY = currentScrollY;
+});
